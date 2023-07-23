27aaba9d0f27d755a12bf5f605f685eebe382341
cgcel <cgc.elvin@outlook.com>
2023-07-23 22:20:23 +0800

    add: add subscribe queue with args demo.

---
+++ Example/Example.SubscribeQueueWithArgs/BasicConsumer.cs
@@ -0,0 +1,17 @@
+﻿using NanoRabbit.Connection;
+using NanoRabbit.Consumer;
+
+namespace Example.QuickStart
+{
+    public class BasicConsumer : RabbitConsumer<string>
+    {
+        public BasicConsumer(string connectionName, string consumerName, RabbitPool pool) : base(connectionName, consumerName, pool)
+        {
+        }
+
+        public override void MessageHandler(string message)
+        {
+            Console.WriteLine($"Receive: {message}");
+        }
+    }
+}
---
+++ Example/Example.SubscribeQueueWithArgs/Program.cs
@@ -0,0 +1,37 @@
+﻿using Example.QuickStart;
+using NanoRabbit.Connection;
+
+var pool = new RabbitPool();
+pool.RegisterConnection(new ConnectOptions("Connection1")
+{
+    ConnectConfig = new()
+    {
+        HostName = "localhost",
+        Port = 5672,
+        UserName = "admin",
+        Password = "admin",
+        VirtualHost = "DATA"
+    },
+    ConsumerConfigs = new List<ConsumerConfig>
+    {
+        new ConsumerConfig("DataBasicQueueConsumer")
+        {
+            QueueName = "BASIC_QUEUE",
+            Arguments = new Dictionary<string, object>
+            {
+                {"x-message-ttl", 259200000 }
+            }
+        }
+    }
+});
+
+var consumer = new BasicConsumer("Connection1", "DataBasicQueueConsumer", pool);
+await Task.Run(async () =>
+{
+    while (true)
+    {
+        Console.WriteLine("Start receiving...");
+        consumer.Receive();
+        await Task.Delay(1000);
+    }
+});
