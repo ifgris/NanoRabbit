8431e2f872f616e21e2812f12d12c5fadddd7741
cgcel <cgc.elvin@outlook.com>
2023-07-25 17:11:18 +0800

    update: add comments, change the constructor code.

--- Src/NanoRabbit/Connection/ConnectOptions.cs
+++ Src/NanoRabbit/Connection/ConnectOptions.cs
@@ -1,5 +1,4 @@
-﻿using RabbitMQ.Client;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace NanoRabbit.Connection
 {
@@ -8,9 +7,15 @@ namespace NanoRabbit.Connection
     /// </summary>
     public class ConnectOptions
     {
-        public ConnectOptions([Required] string connectionName)
+        /// <summary>
+        /// Create a new RabbitMQ Connection.
+        /// </summary>
+        /// <param name="connectionName"></param>
+        /// <param name="config"></param>
+        public ConnectOptions([Required] string connectionName, Action<ConnectOptions> config)
         {
             ConnectionName = connectionName;
+            config(this);
         }
 
         public string ConnectionName { get; set; }
@@ -25,6 +30,15 @@ namespace NanoRabbit.Connection
     /// </summary>
     public class ConnectConfig
     {
+        /// <summary>
+        /// Connection configuration.
+        /// </summary>
+        /// <param name="config"></param>
+        public ConnectConfig(Action<ConnectConfig> config)
+        {
+            config(this);
+        }
+
         /// <summary>
         /// RabbitMQ HostName, default: localhost
         /// </summary>
@@ -52,6 +66,11 @@ namespace NanoRabbit.Connection
     /// </summary>
     public class ConnectUri
     {
+        public ConnectUri(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
         /// <summary>
         /// Amqp connect Uri
         /// </summary>
@@ -63,9 +82,14 @@ namespace NanoRabbit.Connection
     /// </summary>
     public class ProducerConfig
     {
-        public ProducerConfig([Required] string producerName)
+        /// <summary>
+        /// Producer configuration.
+        /// </summary>
+        /// <param name="producerName"></param>
+        public ProducerConfig([Required] string producerName, Action<ProducerConfig> config)
         {
             ProducerName = producerName;
+            config(this);
         }
 
         public string ProducerName { get; set; }
@@ -82,9 +106,15 @@ namespace NanoRabbit.Connection
     /// </summary>
     public class ConsumerConfig
     {
-        public ConsumerConfig([Required] string consumerName)
+        /// <summary>
+        /// Consumer configuration.
+        /// </summary>
+        /// <param name="consumerName"></param>
+        /// <param name="config"></param>
+        public ConsumerConfig([Required] string consumerName, Action<ConsumerConfig> config)
         {
             ConsumerName = consumerName;
+            config(this);
         }
 
         public string ConsumerName { get; set; }
@@ -94,4 +124,47 @@ namespace NanoRabbit.Connection
         public bool AutoDelete { get; set; } = false;
         public IDictionary<string, object>? Arguments { get; set; } = null;
     }
+
+    /// <summary>
+    /// Convenience class providing compile-time names for standard exchange types.
+    /// </summary>
+    /// <remarks>
+    /// Use the static members of this class as values for the
+    /// "exchangeType" arguments for IModel methods such as
+    /// ExchangeDeclare. The broker may be extended with additional
+    /// exchange types that do not appear in this class.
+    /// </remarks>
+    public static class ExchangeType
+    {
+        /// <summary>
+        /// Exchange type used for AMQP direct exchanges.
+        /// </summary>
+        public const string Direct = "direct";
+
+        /// <summary>
+        /// Exchange type used for AMQP fanout exchanges.
+        /// </summary>
+        public const string Fanout = "fanout";
+
+        /// <summary>
+        /// Exchange type used for AMQP headers exchanges.
+        /// </summary>
+        public const string Headers = "headers";
+
+        /// <summary>
+        /// Exchange type used for AMQP topic exchanges.
+        /// </summary>
+        public const string Topic = "topic";
+
+        private static readonly string[] s_all = { Fanout, Direct, Topic, Headers };
+
+        /// <summary>
+        /// Retrieve a collection containing all standard exchange types.
+        /// </summary>
+        public static ICollection<string> All()
+        {
+            return s_all;
+        }
+    }
+
 }
