bad633b3491f624fb3573062185653ce6ee45dd7
cgcel <cgc.elvin@outlook.com>
2023-07-26 22:16:28 +0800

    fix: ExchangeType namespace conflict.

--- Src/NanoRabbit/Producer/RabbitProducer.cs
+++ Src/NanoRabbit/Producer/RabbitProducer.cs
@@ -28,7 +28,7 @@ namespace NanoRabbit.Producer
         {
             using (var channel = _connection.CreateModel())
             {
-                channel.ExchangeDeclare(_producerConfig.ExchangeName, ExchangeType.Topic, durable: true);
+                channel.ExchangeDeclare(_producerConfig.ExchangeName, _producerConfig.Type, durable: true);
                 var properties = channel.CreateBasicProperties();
                 properties.Persistent = true;
 
