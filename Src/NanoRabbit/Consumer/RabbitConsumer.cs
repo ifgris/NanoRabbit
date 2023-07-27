0ec78513bfb4248ae7f099872046a7598de12986
cgcel <cgc.elvin@outlook.com>
2023-07-28 00:04:05 +0800

    add: comments and consume thread

--- Src/NanoRabbit/Consumer/RabbitConsumer.cs
+++ Src/NanoRabbit/Consumer/RabbitConsumer.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace NanoRabbit.Consumer
 {
     /// <summary>
-    /// RabbitConsumer
+    /// RabbitConsumer, can be inherited by custom Consumer
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public abstract class RabbitConsumer<T> : IRabbitConsumer<T>
@@ -16,12 +16,39 @@ namespace NanoRabbit.Consumer
         private readonly IRabbitPool _pool;
         private readonly ConsumerConfig _consumerConfig;
 
+        private readonly Thread _consumeThread;
+
         public RabbitConsumer(string connectionName, string consumerName, IRabbitPool pool)
         {
             _pool = pool;
             _channel = _pool.GetConnection(connectionName).CreateModel();
             _consumerConfig = _pool.GetConsumer(consumerName);
+            _consumeThread = new Thread(ReceiveTask);
+            _consumeThread.Start();
         }
+
+        /// <summary>
+        /// ReceiveTask runs in PublishThread.
+        /// </summary>
+        public void ReceiveTask()
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            while (true)
+            {
+                consumer.Received += (model, ea) =>
+                {
+                    var body = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    var message = JsonConvert.DeserializeObject<T>(body);
+                    if (message != null)
+                    {
+                        MessageHandler(message);
+                    }
+                };
+                _channel.BasicConsume(queue: _consumerConfig.QueueName, autoAck: true, consumer: consumer);
+                Thread.Sleep(1000);
+            }
+        }
+
 
         public void Receive()
         {
