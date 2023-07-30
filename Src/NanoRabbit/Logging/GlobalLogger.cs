ce8af312190a497c2ecd1ded2efb22d35e672563
cgcel <cgc.elvin@outlook.com>
2023-07-30 16:12:41 +0800

    fix: add a static constructor of GlobalLogger

--- Src/NanoRabbit/Logging/GlobalLogger.cs
+++ Src/NanoRabbit/Logging/GlobalLogger.cs
@@ -4,9 +4,23 @@ namespace NanoRabbit.Logging
 {
     public static class GlobalLogger
     {
-        public static ILoggerFactory Factory { get; } = new LoggerFactory();
+        // private static ILoggerFactory Factory { get; } = new LoggerFactory();
+        //
+        // public static ILogger<T> CreateLogger<T>()
+        // {
+        //     return Factory.CreateLogger<T>();
+        // }
+        private static ILoggerFactory Factory { get; } = InitializeLoggerFactory();
 
-        public static ILogger<T> CreateLogger<T>()
+        private static ILoggerFactory InitializeLoggerFactory() 
+        {
+            var loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddConsole();
+            });
+            return loggerFactory;
+        }  
+
+        public static ILogger<T> CreateLogger<T>() 
         {
             return Factory.CreateLogger<T>();
         }
