05d7dffd282e1b3882031fa5a37d4f3807c541d5
cgcel <cgc.elvin@outlook.com>
2023-08-13 23:13:10 +0800

    add: Example.ProducerInConsumer

---
+++ Example/Example.ProducerInConsumer/ConsumeService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using NanoRabbit.Connection;
+
+namespace Example.ProducerInConsumer
+{
+    public class ConsumeService : BackgroundService
+    {
+        private readonly FooFirstQueueConsumer _consumer;
+
+        public ConsumeService(FooFirstQueueConsumer consumer)
+        {
+            _consumer = consumer;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _consumer.StartConsuming();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _consumer.Dispose();
+            return base.StartAsync(cancellationToken);
+        }
+    }
+}
---
+++ Example/Example.ProducerInConsumer/FooFirstQueueConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using NanoRabbit.Connection;
+using NanoRabbit.Consumer;
+
+namespace Example.ProducerInConsumer
+{
+    public class FooFirstQueueConsumer : RabbitConsumer<string>
+    {
+        private readonly ILogger _logger;
+        private readonly BarFirstQueueProducer _barFirstQueueProducer;
+
+        public FooFirstQueueConsumer(string connectionName, string consumerName, IRabbitPool pool, ILogger<RabbitConsumer<string>> logger, BarFirstQueueProducer barFirstQueueProducer) : base(connectionName, consumerName, pool, logger)
+        {
+            _logger = logger;
+            _barFirstQueueProducer = barFirstQueueProducer;
+        }
+
+        public override void MessageHandler(string message)
+        {
+            _logger.LogInformation($"ConsumerLogging: Receive: {message}");
+
+            var newMessage = "new" + message;
+            _barFirstQueueProducer.Enqueue(newMessage);
+        }
+    }
+}
