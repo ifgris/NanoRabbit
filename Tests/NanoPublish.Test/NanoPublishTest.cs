a742de86aa455b4725ff5f176392c2e9fde69819
cgcel <cgc.elvin@outlook.com>
2023-08-28 22:50:13 +0800

    update: NanoPublishTest

--- Tests/NanoPublish.Test/NanoPublishTest.cs
+++ Tests/NanoPublish.Test/NanoPublishTest.cs
@@ -11,26 +11,27 @@ public class NanoPublishTest
     public void TestNanoPublish()
     {
         var pool = new RabbitPool(c => { c.EnableLogging = true; });
-        pool.RegisterConnection(new ConnectOptions("FooConnection", option =>
-        {
-            option.ConnectConfig = new(config =>
-            {
-                config.HostName = "localhost";
-                config.Port = 5672;
-                config.UserName = "admin";
-                config.Password = "admin";
-                config.VirtualHost = "FooHost";
-            });
-            option.ProducerConfigs = new List<ProducerConfig>
-            {
-                new ProducerConfig("FooFirstQueueProducer", c =>
-                {
-                    c.ExchangeName = "FooTopic";
-                    c.RoutingKey = "FooFirstKey";
-                    c.Type = ExchangeType.Topic;
-                })
-            };
-        }));
+        
+        // pool.RegisterConnection(new ConnectOptions("FooConnection", option =>
+        // {
+        //     option.ConnectConfig = new(config =>
+        //     {
+        //         config.HostName = "localhost";
+        //         config.Port = 5672;
+        //         config.UserName = "admin";
+        //         config.Password = "admin";
+        //         config.VirtualHost = "FooHost";
+        //     });
+        //     option.ProducerConfigs = new List<ProducerConfig>
+        //     {
+        //         new ProducerConfig("FooFirstQueueProducer", c =>
+        //         {
+        //             c.ExchangeName = "FooTopic";
+        //             c.RoutingKey = "FooFirstKey";
+        //             c.Type = ExchangeType.Topic;
+        //         })
+        //     };
+        // }));
         
         pool.NanoPublish("FooConnection", "FooFirstQueueProducer", "NanoPublishTest");
     }
