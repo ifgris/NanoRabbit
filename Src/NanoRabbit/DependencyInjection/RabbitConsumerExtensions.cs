ea22163fca158aa442ee519ec936f7fc2bedeb60
cgcel <cgc.elvin@outlook.com>
2023-08-29 21:12:32 +0800

    update: format codes.

--- Src/NanoRabbit/DependencyInjection/RabbitConsumerExtensions.cs
+++ Src/NanoRabbit/DependencyInjection/RabbitConsumerExtensions.cs
@@ -4,84 +4,86 @@ using Microsoft.Extensions.Logging;
 using NanoRabbit.Connection;
 using NanoRabbit.Consumer;
 
-namespace NanoRabbit.DependencyInjection
+namespace NanoRabbit.DependencyInjection;
+
+public static class RabbitConsumerExtensions
 {
-    public static class RabbitConsumerExtensions
+    /// <summary>
+    /// RabbitConsumer Dependency Injection Method (ConnectionName, ConsumerName)
+    /// </summary>
+    /// <typeparam name="TConsumer"></typeparam>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="services"></param>
+    /// <param name="connectionName"></param>
+    /// <param name="consumerName"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddConsumer<TConsumer, TMessage>(this IServiceCollection services,
+        string connectionName, string consumerName)
+        where TConsumer : RabbitConsumer<TMessage>
     {
-        /// <summary>
-        /// RabbitConsumer Dependency Injection Method (ConnectionName, ConsumerName)
-        /// </summary>
-        /// <typeparam name="TConsumer"></typeparam>
-        /// <typeparam name="TMessage"></typeparam>
-        /// <param name="services"></param>
-        /// <param name="connectionName"></param>
-        /// <param name="consumerName"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddConsumer<TConsumer, TMessage>(this IServiceCollection services, string connectionName, string consumerName)
-            where TConsumer : RabbitConsumer<TMessage>
+        services.AddSingleton<TConsumer>(provider =>
         {
-            services.AddSingleton<TConsumer>(provider =>
-            {
-                var logger = provider.GetRequiredService<ILogger<RabbitConsumer<TMessage>>>();
-                var consumer = ActivatorUtilities.CreateInstance<TConsumer>(provider, connectionName, consumerName, provider.GetRequiredService<IRabbitPool>(), logger);
-                
-                return consumer;
-            });
-            return services;
+            var logger = provider.GetRequiredService<ILogger<RabbitConsumer<TMessage>>>();
+            var consumer = ActivatorUtilities.CreateInstance<TConsumer>(provider, connectionName, consumerName,
+                provider.GetRequiredService<IRabbitPool>(), logger);
+
+            return consumer;
+        });
+        return services;
+    }
+
+    /// <summary>
+    /// RabbitConsumer Dependency Injection Method (QueueName)
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="queueName"></param>
+    /// <typeparam name="TConsumer"></typeparam>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <returns></returns>
+    public static IServiceCollection AddConsumer<TConsumer, TMessage>(this IServiceCollection services,
+        string queueName)
+        where TConsumer : RabbitConsumer<TMessage>
+    {
+        services.AddSingleton<TConsumer>(provider =>
+        {
+            var logger = provider.GetRequiredService<ILogger<RabbitConsumer<TMessage>>>();
+            var consumer = ActivatorUtilities.CreateInstance<TConsumer>(provider, queueName,
+                provider.GetRequiredService<IRabbitPool>(), logger);
+
+            return consumer;
+        });
+        return services;
+    }
+
+    /// <summary>
+    /// RabbitConsumer BackgroundService method
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TMsg"></typeparam>
+    private class RabbitConsumerBackgroundService<T, TMsg> : BackgroundService where T : RabbitConsumer<TMsg>
+    {
+        private readonly T _consumer;
+
+        public RabbitConsumerBackgroundService(T consumer)
+        {
+            _consumer = consumer;
         }
 
-        /// <summary>
-        /// RabbitConsumer Dependency Injection Method (QueueName)
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="queueName"></param>
-        /// <typeparam name="TConsumer"></typeparam>
-        /// <typeparam name="TMessage"></typeparam>
-        /// <returns></returns>
-        public static IServiceCollection AddConsumer<TConsumer, TMessage>(this IServiceCollection services,
-            string queueName)
-            where TConsumer : RabbitConsumer<TMessage>
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            services.AddSingleton<TConsumer>(provider =>
-            {
-                var logger = provider.GetRequiredService<ILogger<RabbitConsumer<TMessage>>>();
-                var consumer = ActivatorUtilities.CreateInstance<TConsumer>(provider, queueName, provider.GetRequiredService<IRabbitPool>(), logger);
-                
-                return consumer;
-            });
-            return services;
+            return Task.CompletedTask;
         }
 
-        /// <summary>
-        /// RabbitConsumer BackgroundService method
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TMsg"></typeparam>
-        private class RabbitConsumerBackgroundService<T, TMsg> : BackgroundService where T : RabbitConsumer<TMsg>
+        public override Task StartAsync(CancellationToken cancellationToken)
         {
-            private readonly T _consumer;
+            _consumer.StartConsuming();
+            return base.StartAsync(cancellationToken);
+        }
 
-            public RabbitConsumerBackgroundService(T consumer)
-            {
-                _consumer = consumer;
-            }
-
-            protected override Task ExecuteAsync(CancellationToken stoppingToken)
-            {
-                return Task.CompletedTask;
-            }
-
-            public override Task StartAsync(CancellationToken cancellationToken)
-            {
-                _consumer.StartConsuming();
-                return base.StartAsync(cancellationToken);
-            }
-
-            public override async Task StopAsync(CancellationToken cancellationToken)
-            {
-                await base.StopAsync(cancellationToken);
-                _consumer.Dispose();
-            }
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await base.StopAsync(cancellationToken);
+            _consumer.Dispose();
         }
     }
 }
--- Src/NanoRabbit/DependencyInjection/RabbitProducerExtensions.cs
+++ Src/NanoRabbit/DependencyInjection/RabbitProducerExtensions.cs
@@ -3,29 +3,30 @@ using Microsoft.Extensions.Logging;
 using NanoRabbit.Connection;
 using NanoRabbit.Producer;
 
-namespace NanoRabbit.DependencyInjection
-{
-    public static class RabbitProducerExtensions
-    {
-        /// <summary>
-        /// RabbitProducer Dependency Injection Functions.
-        /// </summary>
-        /// <typeparam name="TProducer"></typeparam>
-        /// <param name="services"></param>
-        /// <param name="connectionName"></param>
-        /// <param name="producerName"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddProducer<TProducer>(this IServiceCollection services, string connectionName, string producerName)
-            where TProducer : RabbitProducer
-        {
-            services.AddSingleton<TProducer>(provider =>
-            {
-                var logger = provider.GetRequiredService<ILogger<RabbitProducer>>();
-                var producer = ActivatorUtilities.CreateInstance<TProducer>(provider, connectionName, producerName, provider.GetRequiredService<IRabbitPool>(), logger);
-                return producer;
-            });
+namespace NanoRabbit.DependencyInjection;
 
-            return services;
-        }
+public static class RabbitProducerExtensions
+{
+    /// <summary>
+    /// RabbitProducer Dependency Injection Functions.
+    /// </summary>
+    /// <typeparam name="TProducer"></typeparam>
+    /// <param name="services"></param>
+    /// <param name="connectionName"></param>
+    /// <param name="producerName"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddProducer<TProducer>(this IServiceCollection services, string connectionName,
+        string producerName)
+        where TProducer : RabbitProducer
+    {
+        services.AddSingleton<TProducer>(provider =>
+        {
+            var logger = provider.GetRequiredService<ILogger<RabbitProducer>>();
+            var producer = ActivatorUtilities.CreateInstance<TProducer>(provider, connectionName, producerName,
+                provider.GetRequiredService<IRabbitPool>(), logger);
+            return producer;
+        });
+
+        return services;
     }
 }
