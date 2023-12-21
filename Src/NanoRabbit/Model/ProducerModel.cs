d8ca7fc175585923c7d7b4fd417eb516343814ed
cgcel <cgc.elvin@outlook.com>
2023-12-21 14:55:59 +0800

    Add ResendMsgModel to generate the resend message.

---
+++ Src/NanoRabbit/Model/ProducerModel.cs
@@ -0,0 +1,32 @@
+﻿namespace NanoRabbit.Model;
+
+/// <summary>
+/// Resend message model
+/// </summary>
+public class ResendMsgModel
+{
+    public List<MsgInfoModel>? MessageList { get; set; }
+}
+
+public class MsgInfoModel
+{
+    /// <summary>
+    /// Message UUID
+    /// </summary>
+    public string? Id { get; set; }
+    
+    /// <summary>
+    /// Failed-sending message generate time
+    /// </summary>
+    public DateTime GenerateTime { get; set; }
+
+    /// <summary>
+    /// Retry count
+    /// </summary>
+    public int RetryCount { get; set; } = 0;
+    
+    /// <summary>
+    /// Message object
+    /// </summary>
+    public object? Message { get; set; }
+}
