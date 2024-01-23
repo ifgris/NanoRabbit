12cabb2e948589839a3f34021710705479508678
cgcel <cgc.elvin@outlook.com>
2024-01-23 15:22:52 +0800

    Add param: consumerCount.

--- Src/NanoRabbit/DependencyInjection/RabbitSubscriberExtensions.cs
+++ Src/NanoRabbit/DependencyInjection/RabbitSubscriberExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NanoRabbit.Consumer;
 
@@ -7,52 +8,92 @@ namespace NanoRabbit.DependencyInjection;
 public static class RabbitSubscriberExtensions
 {
     public static IServiceCollection AddRabbitSubscriber<TSubscriber>(this IServiceCollection services,
-        string consumerName, bool enableLogging = true) where TSubscriber : RabbitSubscriber
+        string consumerName, int consumerCount = 1, bool enableLogging = true) where TSubscriber : RabbitSubscriber
     {
         services.AddHostedService(provider =>
         {
-            if (enableLogging)
+            var hostedServices = new List<IHostedService>();
+
+            for (int i = 0; i < consumerCount; i++)
             {
-                var logger = provider.GetRequiredService<ILogger<RabbitSubscriber>>();
-                var consumer = provider.GetRequiredService<IRabbitConsumer>();
-                var subscriberService =
-                    ActivatorUtilities.CreateInstance<TSubscriber>(provider, consumer, logger, consumerName);
-                return subscriberService;
+                if (enableLogging)
+                {
+                    var logger = provider.GetRequiredService<ILogger<RabbitAsyncSubscriber>>();
+                    var consumer = provider.GetRequiredService<IRabbitConsumer>();
+                    var subscriberService =
+                        ActivatorUtilities.CreateInstance<TSubscriber>(provider, consumer, logger, consumerName);
+                    // return subscriberService;
+                    hostedServices.Add(subscriberService);
+                }
+                else
+                {
+                    var consumer = provider.GetRequiredService<IRabbitConsumer>();
+                    var subscriberService =
+                        ActivatorUtilities.CreateInstance<TSubscriber>(provider, consumer, consumerName);
+                    // return subscriberService;
+                    hostedServices.Add(subscriberService);
+                }
             }
-            else
-            {
-                var consumer = provider.GetRequiredService<IRabbitConsumer>();
-                var subscriberService =
-                    ActivatorUtilities.CreateInstance<TSubscriber>(provider, consumer, consumerName);
-                return subscriberService;
-            }
+
+            return new CompositeHostedService(hostedServices);
         });
 
         return services;
     }
     
     public static IServiceCollection AddRabbitAsyncSubscriber<TAsyncSubscriber>(this IServiceCollection services,
-        string consumerName, bool enableLogging = true) where TAsyncSubscriber : RabbitAsyncSubscriber
+        string consumerName, int consumerCount = 1, bool enableLogging = true) where TAsyncSubscriber : RabbitAsyncSubscriber
     {
         services.AddHostedService(provider =>
         {
-            if (enableLogging)
+            var hostedServices = new List<IHostedService>();
+
+            for (int i = 0; i < consumerCount; i++)
             {
-                var logger = provider.GetRequiredService<ILogger<RabbitAsyncSubscriber>>();
-                var consumer = provider.GetRequiredService<IRabbitConsumer>();
-                var subscriberService =
-                    ActivatorUtilities.CreateInstance<TAsyncSubscriber>(provider, consumer, logger, consumerName);
-                return subscriberService;
+                if (enableLogging)
+                {
+                    var logger = provider.GetRequiredService<ILogger<RabbitAsyncSubscriber>>();
+                    var consumer = provider.GetRequiredService<IRabbitConsumer>();
+                    var subscriberService =
+                        ActivatorUtilities.CreateInstance<TAsyncSubscriber>(provider, consumer, logger, consumerName);
+                    // return subscriberService;
+                    hostedServices.Add(subscriberService);
+                }
+                else
+                {
+                    var consumer = provider.GetRequiredService<IRabbitConsumer>();
+                    var subscriberService =
+                        ActivatorUtilities.CreateInstance<TAsyncSubscriber>(provider, consumer, consumerName);
+                    // return subscriberService;
+                    hostedServices.Add(subscriberService);
+                }
             }
-            else
-            {
-                var consumer = provider.GetRequiredService<IRabbitConsumer>();
-                var subscriberService =
-                    ActivatorUtilities.CreateInstance<TAsyncSubscriber>(provider, consumer, consumerName);
-                return subscriberService;
-            }
+
+            return new CompositeHostedService(hostedServices);
         });
 
         return services;
+    }
+}
+
+public class CompositeHostedService : IHostedService
+{
+    private readonly List<IHostedService> _hostedServices;
+
+    public CompositeHostedService(List<IHostedService> hostedServices)
+    {
+        _hostedServices = hostedServices;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var tasks = _hostedServices.Select(service => service.StartAsync(cancellationToken));
+        await Task.WhenAll(tasks);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        var tasks = _hostedServices.Select(service => service.StopAsync(cancellationToken));
+        await Task.WhenAll(tasks);
     }
 }
