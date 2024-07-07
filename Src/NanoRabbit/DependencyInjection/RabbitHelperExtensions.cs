cfbf723e5b3a43c5e3f68d3d3825f4ad3d5bf45d
cgcel <cgc.elvin@outlook.com>
2024-07-07 23:03:21 +0800

    Add commits and AddKeyedConsumer function.

--- Src/NanoRabbit/DependencyInjection/RabbitHelperExtensions.cs
+++ Src/NanoRabbit/DependencyInjection/RabbitHelperExtensions.cs
@@ -4,11 +4,20 @@ using NanoRabbit.Connection;
 
 namespace NanoRabbit.DependencyInjection
 {
+    /// <summary>
+    /// RabbitHelper extensions.
+    /// </summary>
     public static class RabbitHelperExtensions
     {
+        /// <summary>
+        /// Adds a singleton service of the type specified in IRabbitHelper with a factory specified in implementationFactory to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="builder"></param>
+        /// <returns></returns>
         public static IServiceCollection AddRabbitHelper(this IServiceCollection services, Action<RabbitConfigurationBuilder> builder)
         {
-            var rabbitConfigBuilder = new RabbitConfigurationBuilder(services);
+            var rabbitConfigBuilder = new RabbitConfigurationBuilder();
             builder.Invoke(rabbitConfigBuilder);
             var rabbitConfig = rabbitConfigBuilder.Build();
 
@@ -18,10 +27,18 @@ namespace NanoRabbit.DependencyInjection
             return services;
         }
 
+        /// <summary>
+        /// Adds a keyed singleton service of the type specified in IRabbitHelper with a factory specified in implementationFactory to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="key"></param>
+        /// <param name="builders"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
         public static IServiceCollection AddKeyedRabbitHelper(this IServiceCollection services, string key, Action<RabbitConfigurationBuilder> builders)
         {
 #if NET7_0_OR_GREATER
-            var rabbitConfigBuilder = new RabbitConfigurationBuilder(services);
+            var rabbitConfigBuilder = new RabbitConfigurationBuilder();
             builders.Invoke(rabbitConfigBuilder);
             var rabbitConfig = rabbitConfigBuilder.Build();
 
@@ -36,6 +53,14 @@ namespace NanoRabbit.DependencyInjection
 #endif
         }
 
+        /// <summary>
+        /// Adds a singleton service of the type specified in IRabbitHelper by reading configurations of appsettings.json.
+        /// </summary>
+        /// <typeparam name="TRabbitConfiguration"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static IServiceCollection AddRabbitMqHelperFromAppSettings<TRabbitConfiguration>(this IServiceCollection services, IConfiguration configuration)
             where TRabbitConfiguration : RabbitConfiguration, new()
         {
@@ -51,7 +76,16 @@ namespace NanoRabbit.DependencyInjection
             });
             return services;
         }
-        
+
+        /// <summary>
+        /// Adds a keyed singleton service of the type specified in IRabbitHelper by reading configurations of appsettings.json.
+        /// </summary>
+        /// <typeparam name="TRabbitConfiguration"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="key"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static IServiceCollection AddKeyedRabbitMqHelperFromAppSettings<TRabbitConfiguration>(this IServiceCollection services, string key, IConfiguration configuration)
             where TRabbitConfiguration : RabbitConfiguration, new()
         {
@@ -74,6 +108,14 @@ namespace NanoRabbit.DependencyInjection
 #endif
         }
 
+        /// <summary>
+        /// Add a consumer to specific IRabbitHelper.
+        /// </summary>
+        /// <typeparam name="THandler"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="consumerName"></param>
+        /// <param name="consumers"></param>
+        /// <returns></returns>
         public static IServiceCollection AddRabbitConsumer<THandler>(this IServiceCollection services, string consumerName, int consumers = 1)
             where THandler : class, IMessageHandler
         {
@@ -90,7 +132,16 @@ namespace NanoRabbit.DependencyInjection
 
             return services;
         }
-        
+
+        /// <summary>
+        /// Add a consumer to specific keyed IRabbitHelper.
+        /// </summary>
+        /// <typeparam name="THandler"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="key"></param>
+        /// <param name="consumerName"></param>
+        /// <param name="consumers"></param>
+        /// <returns></returns>
         public static IServiceCollection AddKeyedRabbitConsumer<THandler>(this IServiceCollection services, string key, string consumerName, int consumers = 1)
             where THandler : class, IMessageHandler
         {
@@ -112,6 +163,14 @@ namespace NanoRabbit.DependencyInjection
 #endif
         }
 
+        /// <summary>
+        /// Add a async consumer to specific IRabbitHelper.
+        /// </summary>
+        /// <typeparam name="TAsyncHandler"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="consumerName"></param>
+        /// <param name="consumers"></param>
+        /// <returns></returns>
         public static IServiceCollection AddAsyncRabbitConsumer<TAsyncHandler>(this IServiceCollection services, string consumerName, int consumers = 1)
         where TAsyncHandler : class, IAsyncMessageHandler
         {
@@ -129,6 +188,42 @@ namespace NanoRabbit.DependencyInjection
             return services;
         }
 
+        /// <summary>
+        /// Add a async consumer to specific keyed IRabbitHelper.
+        /// </summary>
+        /// <typeparam name="THandler"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="key"></param>
+        /// <param name="consumerName"></param>
+        /// <param name="consumers"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddKeyedAsyncRabbitConsumer<TAsyncHandler>(this IServiceCollection services, string key, string consumerName, int consumers = 1)
+            where TAsyncHandler : class, IAsyncMessageHandler
+        {
+#if NET7_0_OR_GREATER
+            services.AddKeyedSingleton<TAsyncHandler>(key);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var rabbitMqHelper = serviceProvider.GetRequiredKeyedService<IRabbitHelper>(key);
+            var messageHandler = serviceProvider.GetRequiredKeyedService<TAsyncHandler>(key);
+
+            // todo: check if queue exists.
+            // rabbitMqHelper.DeclareQueue(queueName);
+
+            rabbitMqHelper.AddAsyncConsumer(consumerName, messageHandler.HandleMessageAsync, consumers);
+
+            return services;
+#else
+            throw new NotSupportedException("Keyed services are only supported in .NET 7 and above.");
+#endif
+        }
+
+        /// <summary>
+        /// Get specific IRabbitHelper service by key.
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static IRabbitHelper GetRabbitHelper(this IServiceProvider serviceProvider, string key)
         {
 #if NET7_0_OR_GREATER
