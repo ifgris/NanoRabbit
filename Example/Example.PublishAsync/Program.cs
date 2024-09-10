ca7494d71c473fff4b0ddeef2f57495e38b21667
cgcel <cgc.elvin@outlook.com>
2024-09-10 15:42:06 +0800

    Add Example.PublishAsync.

---
+++ Example/Example.PublishAsync/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using NanoRabbit;
+using NanoRabbit.Connection;
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+
+var logger = loggerFactory.CreateLogger("RabbitHelper");
+
+var rabbitHelper = new RabbitHelper(rabbitConfig: new RabbitConfiguration
+{
+    HostName = "localhost",
+    Port = 5672,
+    VirtualHost = "/",
+    UserName = "admin",
+    Password = "admin",
+    Producers = new List<ProducerOptions> { new ProducerOptions {
+            ProducerName = "FooProducer",
+            ExchangeName = "amq.topic",
+            RoutingKey = "foo.key",
+            Type= ExchangeType.Topic
+        }
+    }
+}, logger);
+
+await rabbitHelper.PublishAsync<string>("FooProducer", "Hello from NanoRabbit");
+
+await rabbitHelper.PublishBatchAsync<string>("FooProducer", new List<string> { "Test 1", "Test 2" });
+
+Console.WriteLine(" Press [enter] to exit.");
